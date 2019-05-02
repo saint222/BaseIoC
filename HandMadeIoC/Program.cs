b6314ff8068e6f7c27375f9186386120affa0caa
Alexandr <saint12maloj@gmail.com>
2019-05-03 00:21:20 +0300

    С поиском нужного конструктора, проверкой на IsPrimitive и Invoke вместо Activator.CreateInstance()

--- HandMadeIoC/Program.cs
+++ HandMadeIoC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,9 @@ namespace HandMadeIoC
 {
     public class Container
     {
-        Dictionary<Type, Func<object>> registrations = new Dictionary<Type, Func<object>>();
+        readonly Dictionary<Type, Func<object>> registrations = new Dictionary<Type, Func<object>>();
 
-        public void Register<TService, TImpl>() where TImpl : TService
+        public void Register<TService, TImpl>() where TImpl : TService // ограничиваем generic
         {
             this.registrations.Add(typeof(TService), () => this.GetInstance(typeof(TImpl)));
         }
@@ -18,25 +19,32 @@ namespace HandMadeIoC
         public object GetInstance(Type serviceType)
         {
             Func<object> creator;
-            if (this.registrations.TryGetValue(serviceType, out creator)) return creator();
-            else if (!serviceType.IsAbstract) return this.CreateInstance(serviceType);
-            else throw new InvalidOperationException("No registration for " + serviceType);
+            if (this.registrations.TryGetValue(serviceType, out creator))
+                return creator();
+            else if (!serviceType.IsAbstract)
+                return this.CreateInstance(serviceType);
+            else 
+                throw new InvalidOperationException("No registration for " + serviceType);
         }
-
+        
         private object CreateInstance(Type implementationType)
         {
-            var ctor = implementationType.GetConstructors().Single();
+            var ctor = GetConstructorWithMaxParametrs(implementationType);
+            if (implementationType.IsPrimitive) // проверяем на примитивность аргумента
+            {
+                throw new ArgumentException("The implementation type is primitive...");  
+            }
             var parameterTypes = ctor.GetParameters().Select(p => p.ParameterType);
-            var dependencies = parameterTypes.Select(t => this.GetInstance(t)).ToArray();
-            return Activator.CreateInstance(implementationType, dependencies);
+            var dependencies = parameterTypes.Select(t => this.GetInstance(t)).ToArray();            
+            return ctor.Invoke(dependencies); // вместо return Activator.CreateInstance(implementationType, dependencies);
         }
         // поиск конструктора с максимальным кол-ом возможных аргументов
-        private System.Reflection.ConstructorInfo GetConstructorWithMaxParametrs(Type implementationType)
+        private ConstructorInfo GetConstructorWithMaxParametrs(Type implementationType)
         {
             var ctor = implementationType.GetConstructors();
             var ctorWithMaxParametrs = ctor.OrderByDescending(r => r.GetParameters().Length).FirstOrDefault();
             return ctorWithMaxParametrs;
-        }
+        }        
     }
     public interface IEngine
     {
@@ -72,9 +80,8 @@ namespace HandMadeIoC
         static void Main(string[] args)
         {
             var container = new Container();
-            container.Register<IEngine, Engine>();
-            //container.Register<Car, Car>();
-            var car = container.GetInstance(typeof(Car)) as Car; // т.к. не женерк, возвращает object
+            container.Register<IEngine, Engine>();            
+            var car = container.GetInstance(typeof(Car)) as Car; // т.к. не generic, возвращает object
             car.Model = "Renault";
             car.Type = "Megane";
             car.GetDescription();
